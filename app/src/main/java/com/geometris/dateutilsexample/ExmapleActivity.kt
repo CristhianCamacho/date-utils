731521ce08964eff83ab692b7c4e6da7f1631399
Cristhian Camacho <ccamacho@Mini-Mac-02.local>
2018-01-30 11:18:32 -0400

    “message”

---
+++ app/src/main/java/com/geometris/dateutilsexample/ExmapleActivity.kt
@@ -0,0 +1,12 @@
+package com.geometris.dateutilsexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExmapleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exmaple)
+    }
+}
